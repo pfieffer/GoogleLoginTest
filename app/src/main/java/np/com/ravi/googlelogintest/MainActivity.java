dab6578fac7db8aa680c68d0ec8e8741c50d5f4b
RaviGarbujaPun <cbpmgr@gmail.com>
2017-11-23 15:55:09 +0545

    Setup required dependencies

--- app/src/main/java/np/com/ravi/googlelogintest/MainActivity.java
+++ app/src/main/java/np/com/ravi/googlelogintest/MainActivity.java
@@ -3,11 +3,27 @@ package np.com.ravi.googlelogintest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 }
